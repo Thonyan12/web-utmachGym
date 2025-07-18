b18b2605e44d57b52016031f1fc707a53bc71059
Thonyan12 <mauriciolima.1206@gmail.com>
2025-07-17 23:08:56 -0500

    xd

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -63,7 +63,7 @@ export class AuthService {
     localStorage.removeItem('token');
     localStorage.removeItem('currentUser');
     this.currentUserSubject.next(null);
-    // ❌ QUITAR ESTA LÍNEA: this.router.navigate(['/']);
+   
   }
 
   // Obtener token
