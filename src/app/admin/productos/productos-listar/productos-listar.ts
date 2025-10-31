581c1d40d8e9abb391be41733f6ddfd0676a3d13
Thonyan12 <mauriciolima.1206@gmail.com>
2025-10-31 11:24:53 -0500

    feat: Enhance product listing and detail views with improved UI and filtering functionality

--- src/app/admin/productos/productos-listar/productos-listar.ts
+++ src/app/admin/productos/productos-listar/productos-listar.ts
@@ -3,10 +3,11 @@ import { ProductosSidebar } from '../productos-sidebar/productos-sidebar';
 import { Producto, Productos } from '../services/productos';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-productos-listar',
-  imports: [ ProductosSidebar, CommonModule, FormsModule],
+  imports: [ ProductosSidebar, CommonModule, FormsModule, RouterLink],
   templateUrl: './productos-listar.html',
   styleUrl: './productos-listar.css'
 })
@@ -15,10 +16,23 @@ export class ProductosListar implements OnInit {
   searchText: string = '';
 
   constructor(private productosService: Productos) { }
+  
   ngOnInit(): void {
     this.productosService.getProductos().subscribe(data => {
       this.productos = data;
     });
+  }
+
+  // Formatear fecha a formato DIA-MES-AÑO
+  formatearFecha(fecha: string): string {
+    if (!fecha) return '';
+    
+    const date = new Date(fecha);
+    const dia = date.getDate().toString().padStart(2, '0');
+    const mes = (date.getMonth() + 1).toString().padStart(2, '0');
+    const anio = date.getFullYear();
+    
+    return `${dia}-${mes}-${anio}`;
   }
 
 }
